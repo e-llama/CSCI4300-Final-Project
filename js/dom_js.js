003c3749e925e729d26282ec18abe4c9e17608f5
e-llama <105138105+e-llama@users.noreply.github.com>
2022-11-14 13:19:14 -0500

    Add files via upload

---
+++ js/dom_js.js
@@ -0,0 +1,74 @@
+//----------------------------------------------
+// Part 1: Greeting: Click Events
+//----------------------------------------------
+/*
+//buttons
+var amButton = document.getElementById("am");
+var pmButton = document.getElementById("pm");
+
+//add eventlistener
+amButton.addEventListener("click", changeAM);
+pmButton.addEventListener("click", changePM);
+
+//AM/PM button eventlistener functions
+function changeAM() {
+    document.getElementById("greeting").innerHTML = "Good Morning!";
+} 
+function changePM() {
+    document.getElementById("greeting").innerHTML = "Good Night!";
+}
+
+//----------------------------------------------
+// Part 2: Switch the Background Color
+//----------------------------------------------
+
+//buttons
+var redButton = document.getElementById("red");
+var yellowButton = document.getElementById("yellow");
+var greenButton = document.getElementById("green");
+var blueButton = document.getElementById("blue");
+
+//add eventlistener
+redButton.addEventListener("click", redBG);
+yellowButton.addEventListener("click", yellowBG);
+greenButton.addEventListener("click", greenBG);
+blueButton.addEventListener("click", blueBG);
+
+//background color changing functions
+function redBG() {
+    document.getElementById("div_color").setAttribute("class", "bg_red");
+}
+function yellowBG() {
+    document.getElementById("div_color").setAttribute("class", "bg_yellow");
+}
+function greenBG() {
+    document.getElementById("div_color").setAttribute("class", "bg_green");
+}
+function blueBG() {
+    document.getElementById("div_color").setAttribute("class", "bg_blue");
+}
+*/
+//----------------------------------------------
+// Part 3: Create a Dynamic List
+//----------------------------------------------
+
+//buttons and input box variables
+var userInput = document.querySelector("#usrInput");
+var inputButton = document.querySelector("#addBtn");
+var inputList = document.querySelector("#myUL");
+
+//add eventlistener
+inputButton.addEventListener("click", addList);
+
+//add to list function
+function addList() {
+    let input = userInput.value;
+    userInput.value = '';
+
+    var node = document.createElement("li");
+    node.setAttribute("class", "flex-item");
+
+    node.appendChild(document.createTextNode(input));
+    
+    inputList.appendChild(node);
+}
